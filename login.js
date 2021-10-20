15d6a2dcab0fcee1d52d8d7e4b102c7cb12cc95a
xiaoyudxz <89781665+xiaoyudxz@users.noreply.github.com>
2021-10-20 21:09:46 +0800

    Add files via upload

--- login.js
+++ login.js
@@ -1,13 +1,1 @@
-function check(){
-      var name=document.getElementById("name").value;
-   var pass=document.getElementById("pass").value;
-   if(name=="admin" && pass=="syadmin666"){
-   alert("登入成功");
-   window.document.f.action="./web.html";
-   window.document.f.submit();
-   }else{
-   alert("用户名或密码错误");
-   
-   }
-   
-   }
+/*Obfuscated by JShaman.com*/function check(){var _0x1bcac9=document['getElementById']('name')['value'];var _0x2d3ed6=document['getElementById']('pass')['value'];if(_0x1bcac9=='admin'&&_0x2d3ed6=='syadmin666'){alert('登入成功');window['document']['f']['action']='./web.html';window['document']['f']['submit']();}else{alert('用户名或密码错误');}}
